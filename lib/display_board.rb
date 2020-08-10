284365f33b947d5d4534694bc68e687c7c5d7ddb
humorous-loop-2081 <kokhobyohannes@yahoo.com>
2020-08-10 23:59:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,40 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board
+  puts "A Tic Tac Toe Board"
+end
+print display_board
+def display_board
+cell = " | | "
+divider = '-----------'
+puts cell
+puts divider
+end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
