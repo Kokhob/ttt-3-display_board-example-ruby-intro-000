30b9e940acbadc6791b54be4157a32095abfc5ad
humorous-loop-2081 <kokhobyohannes@yahoo.com>
2020-08-11 00:03:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,40 +1,8 @@
 def display_board
-  puts "A Tic Tac Toe Board"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
-def display_board
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-def display_board
-  puts "A Tic Tac Toe Board"
-end
-print display_board
-def display_board
-cell = " | | "
-divider = '-----------'
-puts cell
-puts divider
-end
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
+  
